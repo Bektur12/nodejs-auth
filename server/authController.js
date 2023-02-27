03591d095b953499d7f50c7595b3003bc5a0a4ef
Bektur12 <kanybekovbektur22@gmail.com>
2023-02-27 16:11:20 +0600

    hash password with bcrypt

--- server/authController.js
+++ server/authController.js
@@ -1,14 +1,40 @@
+import User from "../server/models/User.js";
+import Role from "../server/models/Role.js";
+import bcrypt from "bcryptjs";
+
 class authController {
   async registration(req, res) {
     try {
-    } catch (e) {}
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "Пользователь с таким именем уже существует" });
+      }
+      const hasPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: "USER" });
+      console.log(userRole, "rollle");
+      const user = new User({
+        username,
+        password: hasPassword,
+        roles: [userRole.value],
+      });
+      await user.save();
+      return res.json({ message: "Пользователь был успешно зарегистрирован" });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: "Registration error" });
+    }
   }
   async login(req, res) {
     try {
-    } catch (e) {}
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: "Login error" });
+    }
   }
   async getUsers(req, res) {
-    res.json("server");
     try {
     } catch (e) {}
   }
