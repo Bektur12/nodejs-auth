325fcd87900cddd796223a55c11b7858705a29b7
Bektur12 <kanybekovbektur22@gmail.com>
2023-02-27 17:18:39 +0600

    finished backend part

--- server/authController.js
+++ server/authController.js
@@ -1,10 +1,26 @@
 import User from "../server/models/User.js";
 import Role from "../server/models/Role.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { validationResult } from "express-validator";
+import secret from "./config.js";
 
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+  return jwt.sign(payload, secret.secret, { expiresIn: "24h" });
+};
 class authController {
   async registration(req, res) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res
+          .status(400)
+          .json({ message: "Ошибка при регистрации", errors });
+      }
       const { username, password } = req.body;
       const candidate = await User.findOne({ username });
       if (candidate) {
@@ -28,13 +44,23 @@ class authController {
     }
   }
   async login(req, res) {
-    try {
-    } catch (e) {
-      console.log(e);
-      res.status(400).json({ message: "Login error" });
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: `Пользователь ${username} не найден` });
     }
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: "Введен неверный пароль" });
+    }
+    const token = generateAccessToken(user._id, user.roles);
+    return res.json({ token });
   }
   async getUsers(req, res) {
+    const users = await User.find();
+    res.json(users);
     try {
     } catch (e) {}
   }
--- server/authRoute.js
+++ server/authRoute.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
 import authController from "./authController.js";
+import { check } from "express-validator";
+import authMiddleware from "../server/middleware/authMiddleware.js";
+import roleMiddleware from "../server/middleware/roleMiddleware.js";
 const router = new Router();
 
-router.post("/registration", authController.registration);
+router.post(
+  "/registration",
+  [
+    check("username", "Имя пользователя не может быть пустым").notEmpty(),
+    check(
+      "password",
+      "Пароль должен быть более 4 и меньше 18 символов"
+    ).isLength({ min: 4, max: 10 }),
+  ],
+  authController.registration
+);
 router.post("/login", authController.login);
-router.get("/users", authController.getUsers);
+router.get(
+  "/users",
+  roleMiddleware(["USER", "ADMIN"]),
+  authController.getUsers
+);
 
 export default router;
